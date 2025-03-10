34b7be243b7081d0ecec938bcf6491dbf933e1fb
scherbringraftery411 <scherbringraftery411@gmail.com>
2025-03-10 00:29:42 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,9 @@
+
+function getRandomJavascriptCode() {
+	var code = "";
+	code += "let myVar = 5; ";
+	code += "if (myVar === 5) { ";
+	code += "console.log('Hello World'); ";
+	code += "} ";
+	return code;
+}
