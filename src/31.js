408322b1e7d36a38259afd6127f2353c087b2afe
scherbringraftery411 <scherbringraftery411@gmail.com>
2025-04-09 21:30:01 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+const randomNumber = getRandomInt(10);
+console.log(randomNumber); // Output: 7
